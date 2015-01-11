373e90c21e6d791c4334eebc9dcc917607c7da5a
Henare Degan <henare.degan@gmail.com>
2015-01-11 11:19:50 +1100

    Don't get detail pages for applications we already have

--- scraper.rb
+++ scraper.rb
@@ -6,6 +6,12 @@ url = 'http://datrack.canterbury.nsw.gov.au/cgi/datrack.pl?status=On Notificatio
 
 def save_applications(application_list)
   application_list.search('.datrack_resultrow_odd,.datrack_resultrow_even').each do |row|
+    council_reference = row.at(:a).inner_text
+    if !(ScraperWiki.select("* from swdata where `council_reference`='#{council_reference}'").empty? rescue true)
+      puts "Skipping already saved record #{council_reference}"
+      next
+    end
+
     info_url = row.at(:a).attr(:href)
     day, month, year = row.at('.datrack_lodgeddate_cell').inner_text.split('/').map { |n| n.to_i }
     address = row.at('.datrack_houseno_cell').inner_text + ' ' +
@@ -15,7 +21,7 @@ def save_applications(application_list)
     detail_page = @agent.get info_url
 
     application = {
-      council_reference: row.at(:a).inner_text,
+      council_reference: council_reference,
       address: address,
       description: detail_page.at('.wh_preview_master').search(:td)[1].inner_text,
       info_url: info_url,
@@ -24,11 +30,7 @@ def save_applications(application_list)
       date_received: Date.new(year, month, day)
     }
 
-    if (ScraperWiki.select("* from data where `council_reference`='#{application[:council_reference]}'").empty? rescue true)
-      ScraperWiki.save_sqlite([:council_reference], application)
-    else
-      puts "Skipping already saved record " + application[:council_reference]
-    end
+    ScraperWiki.save_sqlite([:council_reference], application)
   end
 end
 
